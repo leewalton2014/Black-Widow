599da5f8c4cf1c549b4ed350819f8f586ad14750
lee.walton <w17007224@northumbria.ac.uk>
2019-02-21 17:23:59 +0000

    cart table

--- Design/RD/avengersArena.sql
+++ Design/RD/avengersArena.sql
@@ -56,3 +56,12 @@ CREATE TABLE IF NOT EXISTS `aa_sales` (
   `saleQuantity` varchar(6) default NULL,
   KEY `saleID` (`saleID`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=979;
+
+DROP TABLE IF EXISTS `aa_cart`;
+CREATE TABLE IF NOT EXISTS `aa_cart` (
+  `cartItemID` int(10) NOT NULL auto_increment,
+  `custID` varchar(100) default NULL,
+  `eventID` varchar(400) default NULL,
+  `cartItemQuantity` varchar(6) default NULL,
+  KEY `cartItemID` (`cartItemID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=979;
