e0c632b91a4cc48e4ae76d5c6b4f854f214b58c9
Lee Walton <lee.walton2014@outlook.com>
2019-05-20 18:18:48 +0100

    db updates

--- Design/RD/avengersArena.sql
+++ Design/RD/avengersArena.sql
@@ -1,22 +1,22 @@
-DROP TABLE IF EXISTS 'aa_events';
-CREATE TABLE IF NOT EXISTS 'aa_events' (
-  'eventID' int(10) NOT NULL auto_increment,
-  'eventTitle' varchar(255) default NULL,
-  'eventDescription' varchar(400) default NULL,
-  'eventDate' date default NULL,
-  'eventTime' time default NULL,
-  'typeID' varchar(10) default NULL,
-  'stageID' varchar(10) default NULL,
-  'ticketPrice' decimal(4,2) default NULL,
-  'imgRef' varchar(100) default NULL,
-  PRIMARY KEY ('eventID')
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=100;`
+DROP TABLE IF EXISTS `aa_events`;
+CREATE TABLE IF NOT EXISTS `aa_events` (
+  `eventID` int(10) NOT NULL auto_increment,
+  `eventTitle` varchar(255) default NULL,
+  `eventDescription` varchar(400) default NULL,
+  `eventDate` date default NULL,
+  `eventTime` time default NULL,
+  `typeID` varchar(10) default NULL,
+  `stageID` varchar(10) default NULL,
+  `ticketPrice` decimal(4,2) default NULL,
+  `imgRef` varchar(100) default NULL,
+  PRIMARY KEY (`eventID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=100;
 
-DROP TABLE IF EXISTS 'aa_event_type';
-CREATE TABLE IF NOT EXISTS 'aa_event_type' (
-  'typeID' varchar(10) NOT NULL default '',
-  'eventType' varchar(30) default NULL,
-  PRIMARY KEY  ('typeID')
+DROP TABLE IF EXISTS `aa_event_type`;
+CREATE TABLE IF NOT EXISTS `aa_event_type` (
+  `typeID` varchar(10) NOT NULL default ``,
+  `eventType` varchar(30) default NULL,
+  PRIMARY KEY (`typeID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 INSERT INTO 'aa_event_type' ('typeID', 'eventType') VALUES
@@ -28,12 +28,12 @@ INSERT INTO 'aa_event_type' ('typeID', 'eventType') VALUES
 ('t6', 'Conference'),
 ('t7', 'Other');
 
-DROP TABLE IF EXISTS 'aa_event_stage';
-CREATE TABLE IF NOT EXISTS 'aa_event_stage' (
-  'stageID' varchar(10) NOT NULL default '',
-  'stageNumber' varchar(7) default NULL,
-  'stageCapacity' varchar(7) default NULL,
-  PRIMARY KEY  ('stageID')
+DROP TABLE IF EXISTS `aa_event_stage`;
+CREATE TABLE IF NOT EXISTS `aa_event_stage` (
+  `stageID` varchar(10) NOT NULL default ``,
+  `stageNumber` varchar(7) default NULL,
+  `stageCapacity` varchar(7) default NULL,
+  PRIMARY KEY (`stageID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 INSERT INTO 'aa_event_stage' ('stageID', 'stageNumber', 'stageCapacity') VALUES
@@ -43,68 +43,52 @@ INSERT INTO 'aa_event_stage' ('stageID', 'stageNumber', 'stageCapacity') VALUES
 ('s4', '4', '5000'),
 ('s5', '5', '2000');
 
-DROP TABLE IF EXISTS 'aa_customers';
-CREATE TABLE IF NOT EXISTS 'aa_customers' (
-  'custID' int(10) NOT NULL auto_increment,
-  'custForename' varchar(255) default NULL,
-  'custSurname' varchar(255) default NULL,
-  'custEmail' varchar(255) default NULL,
-  'custUsername' varchar(30) default NULL,
-  KEY 'custID' ('custID')
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=979;
+DROP TABLE IF EXISTS `aa_customers`;
+CREATE TABLE IF NOT EXISTS `aa_customers` (
+  `custID` int(10) NOT NULL auto_increment,
+  `custForename` varchar(255) default NULL,
+  `custSurname` varchar(255) default NULL,
+  `custEmail` varchar(255) default NULL,
+  `custUsername` varchar(30) default NULL,
+  `custPasswordHash` varchar(255) default NULL,
+  PRIMARY KEY (`custID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=979;
 
-DROP TABLE IF EXISTS 'aa_sales';
-CREATE TABLE IF NOT EXISTS 'aa_sales' (
-  'saleID' int(10) NOT NULL auto_increment,
-  'custID' varchar(10) default NULL,
-  'eventID' varchar(10) default NULL,
-  'saleQuantity' varchar(6) default NULL,
-  KEY 'saleID' ('saleID')
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=979;
+DROP TABLE IF EXISTS `aa_sales`;
+CREATE TABLE IF NOT EXISTS `aa_sales` (
+  `saleID` int(10) NOT NULL auto_increment,
+  `custID` varchar(10) default NULL,
+  `eventID` varchar(10) default NULL,
+  `saleQuantity` int(6) default NULL,
+  `orderNumber` varchar(255) default NULL,
+  PRIMARY KEY (`saleID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=979;
 
-DROP TABLE IF EXISTS 'aa_cart';
-CREATE TABLE IF NOT EXISTS 'aa_cart' (
-  'cartItemID' int(10) NOT NULL auto_increment,
-  'custID' varchar(10) default NULL,
-  'eventID' varchar(10) default NULL,
-  'cartItemQuantity' varchar(6) default NULL,
-  KEY 'cartItemID' ('cartItemID')
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=100;
+DROP TABLE IF EXISTS `aa_cart`;
+CREATE TABLE IF NOT EXISTS `aa_cart` (
+  `cartItemID` int(10) NOT NULL auto_increment,
+  `custID` varchar(10) default NULL,
+  `eventID` varchar(10) default NULL,
+  `cartItemQuantity` int(6) default NULL,
+  PRIMARY KEY (`cartItemID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=100;
 
-DROP TABLE IF EXISTS 'aa_blog';
-CREATE TABLE IF NOT EXISTS 'aa_blog' (
-  'postID' int(10) NOT NULL auto_increment,
-  'postTitle' varchar(255) default NULL,
-  'postBody' varchar(10000) default NULL,
-  'postDate' date default NULL,
-  KEY 'postID' ('postID')
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=100;
+DROP TABLE IF EXISTS `aa_blog`;
+CREATE TABLE IF NOT EXISTS `aa_blog` (
+  `postID` int(10) NOT NULL auto_increment,
+  `postTitle` varchar(255) default NULL,
+  `postBody` varchar(10000) default NULL,
+  `postDate` date default NULL,
+  PRIMARY KEY (`postID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=100;
 
-DROP TABLE IF EXISTS 'aa_admins';
-CREATE TABLE IF NOT EXISTS 'aa_admins' (
-  'userID' int(10) NOT NULL auto_increment,
-  'username' varchar(255) default NULL,
-  'passwordHash' varchar(255) default NULL,
-  'forename' varchar(225) default NULL,
-  'surname' varchar(225) default NULL,
-  PRIMARY KEY 'userID' ('userID')
-  UNIQUE KEY 'username' ('username')    
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
-
-DROP TABLE IF EXISTS 'aa_customers';
-CREATE TABLE IF NOT EXISTS 'aa_customers' (
-  'postID' int(10) NOT NULL auto_increment,
-  'postTitle' varchar(255) default NULL,
-  'postBody' varchar(10000) default NULL,
-  'postDate' date default NULL,
-  KEY 'postID' ('postID')
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=100;
-
-DROP TABLE IF EXISTS 'aa_sales';
-CREATE TABLE IF NOT EXISTS 'aa_sales' (
-  'saleID' int(10) NOT NULL auto_increment,
-  'orderNumber' varchar(255) default NULL,
-  'postBody' varchar(10000) default NULL,
-  'postDate' date default NULL,
-  KEY 'saleID' ('saleID')
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=100;
+DROP TABLE IF EXISTS `aa_admins`;
+CREATE TABLE IF NOT EXISTS `aa_admins` (
+  `adminID` int(10) NOT NULL auto_increment,
+  `username` varchar(255) default NULL,
+  `passwordHash` varchar(255) default NULL,
+  `forename` varchar(225) default NULL,
+  `surname` varchar(225) default NULL,
+  PRIMARY KEY (`adminID`)
+  UNIQUE KEY (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=100;
